6db96ebf7d885ee62f694e19fa80c8d47e9bc78f
goranaviani <goranaviani@gmail.com>
2022-08-27 10:18:37 +0200

    section 3

--- src/components/message.js
+++ src/components/message.js
@@ -9,13 +9,21 @@ function changeMessageVisibility (toogleMessage, setToggleMessage) {
 
 }
 
+function toogleMessageFun(toogleMessage, setToggleMessage) {
+    if (toogleMessage === "true"){
+        setToggleMessage("false")}
+    else {
+        setToggleMessage("true")
+    }
+}
+
 function Message ({message}) {
     const [toogleMessage, setToggleMessage] = useState("true")
 
     return (
         <>
             {toogleMessage === "true" && (<>message is : {message}</>)}
-        <button onClick={() =>{setToggleMessage("false")}}>Toggle message visibility</button>        </>
+        <button onClick={() =>{toogleMessageFun(toogleMessage, setToggleMessage)}}>Toggle message visibility</button>        </>
     )
 }
 
